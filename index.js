4554a7154dbefb2ddfbb9b9afb7593b60657dbbd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-28 22:20:38 -0400

    Connects MySQL database to app

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ const mysql = require('mysql');
 
 const app = express();
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.SQL_PASSWORD,
+    database: 'acme'
+});
+
+db.connect();
+
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM users';
+
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(results);
+    });
+});
+
 app.listen(5000, () => console.log('Server for node_mysql_example is live!'));
